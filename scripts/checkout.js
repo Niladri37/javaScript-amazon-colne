db92fbdcca917cb8041d1412c90619aa29a5b6ea
Niladri37 <niladridas413@gmail.com>
2025-02-03 16:05:33 +0530

    Converts from promises to async awiait

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,12 +4,24 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
+
+async function loadPage () {
+  console.log('load page');
+  await loadProductsFetch();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+
+loadPage();
+
+
+/*
 loadProductsFetch()
 .then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 })
-
+*/
 /*
 loadProducts(() => {
   renderOrderSummary();
